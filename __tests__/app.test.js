11388b7d758a6ecb41ab4555afb0b98c9e2e963b
m-ghiringhelli <mghiring@gmail.com>
2022-06-23 16:01:35 -0700

    added test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('github oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should redirect to github oauth page on login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
+
   afterAll(() => {
     pool.end();
   });
