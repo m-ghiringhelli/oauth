49ffa2de5ce9aac275f39725e0edbcaf78e6cee6
m-ghiringhelli <mghiring@gmail.com>
2022-06-24 15:06:06 -0700

    test for login and redirect

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github');
+
 describe('github oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -14,6 +16,23 @@ describe('github oauth routes', () => {
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+
+  it('should login and redirect to posts', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=12')
+      .redirects(1);
+
+    console.log('res', res.body);
+    
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_user',
+      email: 'fake_email@fake.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 
   afterAll(() => {
